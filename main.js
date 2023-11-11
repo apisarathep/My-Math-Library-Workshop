7545bde4b3dbea2e5b29431e805120d6782d9857
Apisara Thepsing <apisara.the@dome.tu.ac.th>
2023-11-11 11:01:34 +0700

    Add change array functionality

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 function sum(a,b){
-    if(Array.isArray(a)){
-        let total =0;
-        for (let i =0; i < a.length; i++){
-            total += a[i];
-        }
-        return total
-    } else if (typeof a=== 'number' && typeof b === 'number'){
+    if (Array.isArray(a)){
+        return a.reduce((acc, n) => acc + n, 0)
+    } else if (typeof a === 'number' && typeof b === 'number'){
         return a+b;
     }
 }
